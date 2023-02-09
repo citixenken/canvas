3150cb382d2c93cc904c17f5ae0a5ca570038fdc
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-09 23:25:40 +0300

    feat: rendering and connecting our data

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { getList } from "./api/picsum";
+import { actionCreators, initialState, reducer } from "./reducers/photos";
+import PhotoGrid from "./components/PhotoGrid";
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { loading, error, photos, nextPage } = state;
+
+  const fetchPhotos = useCallback(async () => {
+    dispatch(actionCreators.loading());
+
+    try {
+      const nextPhotos = await getList(nextPage);
+      dispatch(actionCreators.success(nextPhotos, nextPage));
+    } catch (e) {
+      dispatch(actionCreators.failure());
+    }
+  }, [nextPage]);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  // show error if first page fails to load
+  if (photos.length === 0) {
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator animating={true} />
+        </View>
+      );
+    }
+
+    if (error) {
+      return (
+        <View style={styles.container}>
+          <Text>Failed to load photos!</Text>
+        </View>
+      );
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />
   );
 }
 
