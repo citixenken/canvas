cf520174fff24d53c674663d38281f1f7992cb8f
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-09 22:33:42 +0300

    feat: add initialState obj and reducer fn

--- reducers/photos.js
+++ reducers/photos.js
@@ -8,3 +8,27 @@ export const actionCreators = {
     payload: { photos, page },
   }),
 };
+
+export const initialState = {
+  loading: false,
+  error: false,
+  photos: [],
+  nextPage: 1,
+};
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case types.LOADING:
+      return { ...state, loading: true, error: false };
+    case types.SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        photos: [...state.photos, ...action.payload.photos],
+        nextPage: state.nextPage + 1,
+      };
+    case types.FAILURE:
+      return { ...state, loading: false, error: true };
+  }
+}
