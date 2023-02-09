ea2cc0b81fc1e7593bb601c3a4863f2918dec7a3
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-09 13:37:36 +0300

    feat: handling external API call

--- App.js
+++ App.js
@@ -3,17 +3,9 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-
-    return () => clearInterval(id);
-  }, []);
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 100 }}>{count}</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
